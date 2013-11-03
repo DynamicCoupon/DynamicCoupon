4dca1ad08b5e6999c1237dd8cbc92c2ceb02ac9b
Paul Shi <hipaulshi@gmail.com>
2013-11-02 21:46:21 -0700

    fix html server error , now can work without .jade

--- app.js
+++ app.js
@@ -37,15 +37,18 @@ var usermodel = mongoose.model('user',userScheme);
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
+// app.use(express.static(__dirname + '/public'));
 
 // development only
 if ('development' == app.get('env')) {
@@ -103,6 +106,13 @@ server.send({
 };
 
 app.get('/', routes.index);
+
+
+app.get('/temp', function(req,res){
+    res.render( 'images.html');
+
+});
+
 app.get('/users', user.list);
 function addUser(email,myShareLink){
 	var newitem = new usermodel({
